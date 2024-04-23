2b32d6bb71176afc5bccd3514b826418bfb0fb7b
Antonin Blot <antonin.blot@crick.ac.uk>
2024-04-23 22:47:36 +0100

    [style] run pre-commit

--- iss_analysis/classify.py
+++ iss_analysis/classify.py
@@ -7,64 +7,78 @@ from .io import load_data_tasic_2018
 from itertools import cycle
 
 
-def classify_cells(masks_file, spots_data, ref_data, opts=None, filter_neurons=True,
-                   classify_by='cluster'):
+def classify_cells(
+    masks_file,
+    spots_data,
+    ref_data,
+    opts=None,
+    filter_neurons=True,
+    classify_by="cluster",
+):
     if opts is None:
         opts = {}
-        opts['Inefficiency'] = 0.001
-        opts['MisreadDensity'] = 0.0000001
-        opts['SpotReg'] = 0.01
+        opts["Inefficiency"] = 0.001
+        opts["MisreadDensity"] = 0.0000001
+        opts["SpotReg"] = 0.01
 
     masks = np.load(masks_file, allow_pickle=True)
     spots = pd.read_pickle(spots_data)
-    spots.rename(columns = {'gene':'Gene'}, inplace = True)
-    gene_names = spots['Gene'].unique()
+    spots.rename(columns={"gene": "Gene"}, inplace=True)
+    gene_names = spots["Gene"].unique()
     exons_df, genes = load_data_tasic_2018(ref_data, filter_neurons=filter_neurons)
-    sc_data = exons_df.set_index(classify_by).filter(regex='\d').set_axis(genes, axis=1, inplace=False)
+    sc_data = (
+        exons_df.set_index(classify_by)
+        .filter(regex="\d")
+        .set_axis(genes, axis=1, inplace=False)
+    )
 
-    cell_data, gene_data = pci.fit(spots, coo_matrix(masks), sc_data[gene_names].T, opts=opts)
-    cell_data['BestClass'] = cell_data.apply(lambda r: r['ClassName'][np.argmax(r['Prob'])], axis=1)
-    cell_data['BestProb'] = cell_data.apply(lambda r: np.max(r['Prob']), axis=1)
+    cell_data, gene_data = pci.fit(
+        spots, coo_matrix(masks), sc_data[gene_names].T, opts=opts
+    )
+    cell_data["BestClass"] = cell_data.apply(
+        lambda r: r["ClassName"][np.argmax(r["Prob"])], axis=1
+    )
+    cell_data["BestProb"] = cell_data.apply(lambda r: np.max(r["Prob"]), axis=1)
     return cell_data, gene_data
 
 
 def plot_cell_types(cell_data):
     colors = {
-        'L2/3': 'deepskyblue',
-        'L4': 'dodgerblue',
-        'L5 IT': 'blue',
-        'L5 NP': 'magenta',
-        'L5 PT': 'blueviolet',
-        'L6 CT':  'forestgreen',
-        'L6 IT': 'violet',
-        'L6b': 'black',
-        'Pvalb': 'darkorange',
-        'Sst':  'orangered',
-        'Sncg': 'deeppink',
-        'Serpinf1': 'limegreen',
-        'Lamp5': 'tomato',
-        'Vip': 'crimson'
+        "L2/3": "deepskyblue",
+        "L4": "dodgerblue",
+        "L5 IT": "blue",
+        "L5 NP": "magenta",
+        "L5 PT": "blueviolet",
+        "L6 CT": "forestgreen",
+        "L6 IT": "violet",
+        "L6b": "black",
+        "Pvalb": "darkorange",
+        "Sst": "orangered",
+        "Sncg": "deeppink",
+        "Serpinf1": "limegreen",
+        "Lamp5": "tomato",
+        "Vip": "crimson",
     }
-    markers = cycle('ov^<>spPXD*')
-    plt.figure(figsize=(15,15))
-    zero_class = cell_data[cell_data['BestClass'] == 'Zero']
-    cell_data = cell_data[cell_data['BestClass'] != 'Zero']
-    ax = plt.subplot(1,1,1)
-    clusters = np.sort(cell_data['BestClass'].unique())
+    markers = cycle("ov^<>spPXD*")
+    plt.figure(figsize=(15, 15))
+    zero_class = cell_data[cell_data["BestClass"] == "Zero"]
+    cell_data = cell_data[cell_data["BestClass"] != "Zero"]
+    ax = plt.subplot(1, 1, 1)
+    clusters = np.sort(cell_data["BestClass"].unique())
     for i, cluster in enumerate(clusters):
-        color = 'gray'
+        color = "gray"
         for type in colors:
             if cluster.find(type) != -1:
                 color = colors[type]
         plt.plot(
-            cell_data[cell_data['BestClass'] == cluster]['X'],
-            cell_data[cell_data['BestClass'] == cluster]['Y'],
+            cell_data[cell_data["BestClass"] == cluster]["X"],
+            cell_data[cell_data["BestClass"] == cluster]["Y"],
             next(markers),
             c=color,
-            markersize=10
+            markersize=10,
         )
 
-    plt.legend(clusters, loc='right', ncol=2, bbox_to_anchor=(0.,0.,1.5,1.))
-    plt.plot(zero_class['X'], zero_class['Y'], 'o', markersize=5, c='gray')
-    ax.set_aspect('equal', 'box')
+    plt.legend(clusters, loc="right", ncol=2, bbox_to_anchor=(0.0, 0.0, 1.5, 1.0))
+    plt.plot(zero_class["X"], zero_class["Y"], "o", markersize=5, c="gray")
+    ax.set_aspect("equal", "box")
     ax.invert_yaxis()
--- iss_analysis/io.py
+++ iss_analysis/io.py
@@ -7,10 +7,10 @@ import h5py
 
 def filter_genes(gene_names):
     # get rid of gene models etc
-    genes_Rik = np.array([ re.search('Rik$', s) is not None for s in gene_names ])
-    genes_Gm = np.array([ re.search('Gm\d', s) is not None for s in gene_names ])
-    genes_LOC = np.array([ re.search('LOC\d', s) is not None for s in gene_names ])
-    genes_AA = np.array([ re.search('^[A-Z]{2}\d*$', s) is not None for s in gene_names ])
+    genes_Rik = np.array([re.search("Rik$", s) is not None for s in gene_names])
+    genes_Gm = np.array([re.search("Gm\d", s) is not None for s in gene_names])
+    genes_LOC = np.array([re.search("LOC\d", s) is not None for s in gene_names])
+    genes_AA = np.array([re.search("^[A-Z]{2}\d*$", s) is not None for s in gene_names])
     keep_genes = np.logical_not(genes_Rik + genes_Gm + genes_LOC + genes_AA)
     return keep_genes
 
@@ -32,36 +32,32 @@ def load_data_tasic_2018(datapath, filter_neurons=True):
         gene_names: pandas.Series of gene names
 
     """
-    fname_metadata = f'{datapath}mouse_VISp_2018-06-14_samples-columns.csv'
+    fname_metadata = f"{datapath}mouse_VISp_2018-06-14_samples-columns.csv"
     metadata = pd.read_csv(fname_metadata, low_memory=False)
-    fname = f'{datapath}mouse_VISp_2018-06-14_exon-matrix.csv'
+    fname = f"{datapath}mouse_VISp_2018-06-14_exon-matrix.csv"
     exons = pd.read_csv(fname, low_memory=False)
-    fname_genes = f'{datapath}mouse_VISp_2018-06-14_genes-rows.csv'
+    fname_genes = f"{datapath}mouse_VISp_2018-06-14_genes-rows.csv"
     genes = pd.read_csv(fname_genes, low_memory=False)
-    gene_names = genes['gene_symbol']
-    metadata.set_index('sample_name', inplace=True)
+    gene_names = genes["gene_symbol"]
+    metadata.set_index("sample_name", inplace=True)
     keep_genes = filter_genes(gene_names)
     exons = exons.iloc[keep_genes]
     gene_names = gene_names.iloc[keep_genes]
-    exons_df = metadata.join(exons.T, on='sample_name')
+    exons_df = metadata.join(exons.T, on="sample_name")
     # only include neurons
-    include_classes = [
-        'GABAergic',
-        'Glutamatergic'
-    ]
+    include_classes = ["GABAergic", "Glutamatergic"]
     # get rid of low quality cells etc
     if filter_neurons:
-        exons_subset = exons_df[exons_df['class'].isin(include_classes)]
+        exons_subset = exons_df[exons_df["class"].isin(include_classes)]
     else:
         exons_subset = exons_df
-    exons_subset = exons_subset[~exons_subset['cluster'].str.contains('ALM')]
-    exons_subset = exons_subset[~exons_subset['cluster'].str.contains('Doublet')]
-    exons_subset = exons_subset[~exons_subset['cluster'].str.contains('Batch')]
-    exons_subset = exons_subset[~exons_subset['cluster'].str.contains('Low Quality')]
-    exons_subset = exons_subset[~exons_subset['subclass'].str.contains('High Intronic')]
+    exons_subset = exons_subset[~exons_subset["cluster"].str.contains("ALM")]
+    exons_subset = exons_subset[~exons_subset["cluster"].str.contains("Doublet")]
+    exons_subset = exons_subset[~exons_subset["cluster"].str.contains("Batch")]
+    exons_subset = exons_subset[~exons_subset["cluster"].str.contains("Low Quality")]
+    exons_subset = exons_subset[~exons_subset["subclass"].str.contains("High Intronic")]
 
     return exons_subset, gene_names
-
 
 
 def load_data_yao_2021(datapath):
@@ -81,54 +77,70 @@ def load_data_yao_2021(datapath):
         gene_names: pandas.Series of gene names
 
     """
-    def extract_sparse_matrix(h5f, data_path):
-        """ Load HDF5 data as a sparse matrix """
-        data = h5f[data_path]
-        x = data['x']
-        i = data['i']
-        p = data['p']
-        dims = data['dims']
 
-        sparse_matrix = ss.csc_matrix((x[0:x.len()],
-                                       i[0:i.len()],
-                                       p[0:p.len()]),
-                                      shape=(dims[0], dims[1]),
-                                      dtype=np.int32)
+    def extract_sparse_matrix(h5f, data_path):
+        """Load HDF5 data as a sparse matrix"""
+        data = h5f[data_path]
+        x = data["x"]
+        i = data["i"]
+        p = data["p"]
+        dims = data["dims"]
+
+        sparse_matrix = ss.csc_matrix(
+            (x[0 : x.len()], i[0 : i.len()], p[0 : p.len()]),
+            shape=(dims[0], dims[1]),
+            dtype=np.int32,
+        )
         return sparse_matrix
 
-    fname = f'{datapath}expression_matrix.hdf5'
+    fname = f"{datapath}expression_matrix.hdf5"
 
-    h5f = h5py.File(fname, 'r')
+    h5f = h5py.File(fname, "r")
 
-    exons = extract_sparse_matrix(h5f, '/data/exon')
-    samples = [ sample.decode('utf-8') for sample in h5f['sample_names'] ]
-    gene_names = [ gene.decode('utf-8') for gene in h5f['gene_names'] ]
+    exons = extract_sparse_matrix(h5f, "/data/exon")
+    samples = [sample.decode("utf-8") for sample in h5f["sample_names"]]
+    gene_names = [gene.decode("utf-8") for gene in h5f["gene_names"]]
     keep_genes = filter_genes(gene_names)
     gene_names = np.array(gene_names)[keep_genes]
     exons = exons[:, keep_genes]
 
-    fname_metadata = f'{datapath}metadata.csv'
+    fname_metadata = f"{datapath}metadata.csv"
 
     metadata = pd.read_csv(fname_metadata, low_memory=False)
     include_classes = [
-        'L5 PT CTX', 'L5 IT CTX', 'L4/5 IT CTX', 'L6 IT CTX', 'L6 CT CTX',
-        'L5/6 NP CTX', 'Pvalb', 'Vip', 'L2/3 IT CTX', 'Lamp5', 'Sst',
-        'Sst Chodl', 'Sncg', 'Car3', 'L6b CTX', 'CR', 'Meis2'
+        "L5 PT CTX",
+        "L5 IT CTX",
+        "L4/5 IT CTX",
+        "L6 IT CTX",
+        "L6 CT CTX",
+        "L5/6 NP CTX",
+        "Pvalb",
+        "Vip",
+        "L2/3 IT CTX",
+        "Lamp5",
+        "Sst",
+        "Sst Chodl",
+        "Sncg",
+        "Car3",
+        "L6b CTX",
+        "CR",
+        "Meis2",
     ]
     metadata = metadata[
-        (metadata['region_label'] == 'VISp') &
-        (metadata['subclass_label'].isin(include_classes)) &
-        (metadata['sample_name'].isin(samples))
-        ]
-    keep_cells = np.array([ sample in metadata['sample_name'].unique() for sample in samples ])
+        (metadata["region_label"] == "VISp")
+        & (metadata["subclass_label"].isin(include_classes))
+        & (metadata["sample_name"].isin(samples))
+    ]
+    keep_cells = np.array(
+        [sample in metadata["sample_name"].unique() for sample in samples]
+    )
     samples = np.array(samples)[keep_cells]
     exons = exons[keep_cells, :]
     exons_df = pd.DataFrame(
-        exons.todense(),
-        columns=['gene_' + gene for gene in gene_names]
+        exons.todense(), columns=["gene_" + gene for gene in gene_names]
     )
 
-    exons_df['sample_name'] = samples
-    exons_df = metadata.join(exons_df.set_index('sample_name'), on='sample_name')
+    exons_df["sample_name"] = samples
+    exons_df = metadata.join(exons_df.set_index("sample_name"), on="sample_name")
 
     return exons_df, pd.Series(gene_names)
