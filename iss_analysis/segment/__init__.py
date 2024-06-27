3e3afa9ab9c48ccee394dcdc131d9e080fbde223
Antonin Blot <antonin.blot@crick.ac.uk>
2024-06-27 09:04:34 +0100

    [minor] expose match_starter_to_barcodes in init

--- iss_analysis/segment/__init__.py
+++ iss_analysis/segment/__init__.py
@@ -3,4 +3,8 @@
 Mostly used to attributes spots to cells
 """
 
-from .segment import get_barcode_in_cells, save_stitched_for_manual_clicking
+from .segment import (
+    get_barcode_in_cells,
+    save_stitched_for_manual_clicking,
+    match_starter_to_barcodes,
+)
