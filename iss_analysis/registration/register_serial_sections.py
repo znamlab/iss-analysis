9a4e6acb9958d1cd4796a8da836653cc80683cb5
Antonin Blot <antonin.blot@crick.ac.uk>
2024-07-30 16:17:00 +0100

    [feature] improve register_local_spots
    still WIP.
    - Add "N barcodes used" output.
    - debug mode to return intermediate steps.
    - move center_point as first input to loop on cells

--- iss_analysis/registration/register_serial_sections.py
+++ iss_analysis/registration/register_serial_sections.py
@@ -8,15 +8,16 @@ from . import ara_registration
 
 
 def register_local_spots(
+    center_point: np.array,
     spot_df: pd.DataFrame,
     ref_slice: str,
     target_slice: str,
-    center_point: np.array,
     window_size: float,
     min_spots: int = 5,
     max_barcode_number: int = 500,
     gaussian_width: float = 30,
     verbose: bool = True,
+    debug: bool = False,
 ):
     """Register spots in two serial sections using phase correlation
 
@@ -26,10 +27,10 @@ def register_local_spots(
     two slices.
 
     Args:
+        center_point (np.array): Center point in ARA coordinates
         spot_df (pd.DataFrame): DataFrame with spots.
         ref_slice (str): Reference slice name (format `{chamber}_{roi:02d}`)
         target_slice (str): Target slice name (format `{chamber}_{roi:02d}`)
-        center_point (np.array): Center point in ARA coordinates
         window_size (float): Window size in um
         min_spots (int, optional): Minimum number of spots in common. Defaults to 5.
         max_barcode_number (int, optional): Maximum number of barcodes to consider.
@@ -37,21 +38,19 @@ def register_local_spots(
         gaussian_width (float, optional): Width of the gaussian kernel for spot images.
             Defaults to 30.
         verbose (bool, optional): Print progress. Defaults to True.
+        debug (bool, optional): Return additional information. Defaults to False.
 
     Returns:
         np.array: Shift between the two slices
         float: Maximum correlation value
-        np.array: Phase correlation results
-        np.array: Spot images
-        pd.Index: Selected barcodes
+        np.array: Phase correlation results, if debug is True
+        np.array: Spot images, if debug is True
+        pd.Index: Selected barcodes, if debug is True
 
     """
-
+    center_point = np.asarray(center_point).astype(float)
     if "ara_y_rot" not in spot_df.columns:
-        transform = ara_registration.get_ara_to_slice_rotation_matrix(spot_df)
-        spot_df = ara_registration.rotate_ara_coordinate_to_slice(
-            spot_df, transform=transform
-        )
+        spot_df = ara_registration.rotate_ara_coordinate_to_slice(spot_df)
     if "slice" not in spot_df.columns:
         spot_df["slice"] = (
             spot_df.chamber + "_" + spot_df["roi"].map(lambda x: f"{x:02d}")
@@ -76,39 +75,39 @@ def register_local_spots(
             )
         spots_by_roi.append(spots)
         barcodes_by_roi.append(set(spots.corrected_bases.unique()))
-    if verbose:
-        print(f"Found {len(spots)} spots in the surrounding slice")
-
     barcodes = barcodes_by_roi[0].intersection(barcodes_by_roi[1])
     if verbose:
         print(
             f"Found {len(barcodes)} barcodes in common (intersection of "
             + f"{len(barcodes_by_roi[0])} and {len(barcodes_by_roi[1])})"
         )
-
     # select the barcodes that are present in both slices in large numbers
     spots = pd.concat(spots_by_roi)
+    if verbose:
+        print(f"Found {len(spots)} spots in the surrounding slice")
     spots = spots.query("corrected_bases in @barcodes")
     bc_per_roi = spots.groupby(["slice", "corrected_bases"]).size().unstack().fillna(0)
     best_barcodes = bc_per_roi.min(axis=0).sort_values(ascending=False)
     best_barcodes = best_barcodes[best_barcodes > min_spots]
     if len(best_barcodes) > max_barcode_number:
         best_barcodes = best_barcodes.head(max_barcode_number)
-    bc_per_roi[best_barcodes.index]
 
-    spots = spots.query("corrected_bases in @best_barcodes.index")
+    spots = spots.query("corrected_bases in @best_barcodes.index").copy()
+    spots["convolution_y"] = (spots.ara_y_rot * 1000).astype(int)
+    spots["convolution_z"] = (spots.ara_z_rot * 1000).astype(int)
     if verbose:
         print(
             f"Found {len(spots)} spots in the pair of slices with the selected "
             + f"{len(best_barcodes)} barcodes"
         )
+    if len(best_barcodes) == 0:
+        out = np.empty(2) * np.nan, np.nan, np.nan
+        if debug:
+            out += (np.zeros((0, 2, 0, 0)), np.zeros((0, 0, 0)), pd.Index([]))
+        return out
 
-    origin = np.array([spots.ara_y_rot.min(), spots.ara_z_rot.min()])
-    corner = np.array([spots.ara_y_rot.max(), spots.ara_z_rot.max()])
-    # for the corner we need to add enough space for the kernel to fit
-    corner += (1 + gaussian_width * 20) / 1000
-
-    output_shape = ((corner - origin) * 1000).astype(int)
+    origin = np.array([spots.convolution_y.min(), spots.convolution_z.min()])
+    output_shape = (np.array([2, 2]) * window_size + 1).astype(int)
 
     spot_images = np.empty((len(best_barcodes), 2, *output_shape), dtype="single")
     if verbose:
@@ -120,9 +119,9 @@ def register_local_spots(
         for islice, (slice, slice_df) in enumerate(bc_df.groupby("slice")):
             # rename to x, y for make_spot_image
             sp = pd.DataFrame(
-                slice_df[["ara_y_rot", "ara_z_rot"]].values - origin, columns=["x", "y"]
+                slice_df[["convolution_y", "convolution_z"]].values - origin,
+                columns=["x", "y"],
             )
-            sp *= 1000
             img = make_spot_image(
                 sp,
                 gaussian_width=gaussian_width,
@@ -151,5 +150,7 @@ def register_local_spots(
     shift = argmax - np.array(sum_corr.shape) // 2
     if verbose:
         print(f"Max correlation: {maxcorr} at shift {shift}")
-
-    return shift, maxcorr, phase_corrs, spot_images, best_barcodes.index
+    out = shift, maxcorr, len(best_barcodes)
+    if debug:
+        out += phase_corrs, spot_images, best_barcodes.index
+    return out
